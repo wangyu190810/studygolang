0fe281e3b5f079f08decd17648a7fa4b588b2c71
wangyu190810 <190810401@qq.com>
2019-03-29 16:28:01 +0800

    学习 beego 框架

---
+++ beeHello/routers/router.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"beeHello/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/hello", &controllers.HelloController{})
+}
